933369508493157c9ba625624677d7c0b7505d1d
Eldar <eldarweb@mail.ru>
2023-01-29 23:40:29 +0300

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -37,7 +37,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_15_044247) do
   create_table "rooms", force: :cascade do |t|
     t.string "title"
     t.jsonb "data"
-    t.bigint "project_id"
+    t.bigint "project_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["project_id"], name: "index_rooms_on_project_id"
