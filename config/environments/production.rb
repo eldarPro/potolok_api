8f11a10c6c65329366d7a5c016514d11e29254fe
Eldar <eldarweb@mail.ru>
2023-05-01 23:21:47 +0300

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-   config.hosts << "potolokapi-production.up.railway.app"
+  config.hosts << "potolokapi-production-2237.up.railway.app"
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
