983356bbb196783d5f8f5476980815ea1d73832e
Eldar <eldarweb@mail.ru>
2023-05-01 22:55:45 +0300

    fix

--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,6 @@ ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
+require 'rails/commands/server'
+
+Rails::Server.new(app: 'my_app', Host: '0.0.0.0', Port: 3000).start
